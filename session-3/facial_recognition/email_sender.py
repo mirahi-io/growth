37e1c117191b2984cda7f8bbb7621f1be70d780f
sam <sam@mirahi.io>
2021-10-08 17:58:06 +0200

    face reco azure

--- session-3/facial_recognition/email_sender.py
+++ session-3/facial_recognition/email_sender.py
@@ -7,34 +7,39 @@ from email.mime.base import MIMEBase
 from email import encoders
 import os.path
 
-sender = 'smtp2go.o530o@8shield.net'
-receiver = 'sam@mirahi.io'
-subject = 'Mirahi growth test email '
-file_location = 'session-3/facial_recognition/resources/out.mpeg'
-user = '8shield.net'
-password = 'cG1sZThlNjhrbTAw'
+_user = '8shield.net'
+_password = 'cG1sZThlNjhrbTAw'
 
-msg = MIMEMultipart()
-msg['Subject'] = subject
-msg['From'] = sender
-msg['To'] = receiver
-msg['Subject'] = subject
+def alertAdmin(sender, receiver,subject , file_location):
 
+    sender =  sender
+    receiver = receiver
+    subject = subject
+    file_location = file_location
+    
 
-filename = os.path.basename(file_location)
-attachment = open(file_location, "rb")
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(attachment.read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+    
 
 
-msg.attach(part)
+    filename = os.path.basename(file_location)
+    attachment = open(file_location, "rb")
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
 
-server = smtplib.SMTP('mail.smtp2go.com', 2525)
-server.starttls()
-server.login(user, password)
-text = msg.as_string()
-server.sendmail(sender, receiver, text)
-server.quit()
+
+    msg.attach(part)
+
+    server = smtplib.SMTP('mail.smtp2go.com', 2525)
+    server.starttls()
+    server.login(_user, _password)
+    text = msg.as_string()
+    server.sendmail(sender, receiver, text)
+    server.quit()
+    print('email send')
 
